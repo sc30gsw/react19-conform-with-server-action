7da97334cb6ee99ed2cda71166b373655b105ad2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-08 05:14:29 +0900

    🎉 first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,76 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config = {
+  darkMode: ['class'],
+  content: [ "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}", ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: 'hsl(var(--light))',
+        dark: 'hsl(var(--dark))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        toggle: 'hsl(var(--toggle))',
+        bg: 'hsl(var(--bg))',
+        fg: 'hsl(var(--fg))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          fg: 'hsl(var(--primary-fg))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          fg: 'hsl(var(--secondary-fg))'
+        },
+        tertiary: {
+          DEFAULT: 'hsl(var(--tertiary))',
+          fg: 'hsl(var(--tertiary-fg))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          fg: 'hsl(var(--accent-fg))',
+          subtle: 'hsl(var(--accent-subtle))',
+          'subtle-fg': 'hsl(var(--accent-subtle-fg))'
+        },
+        success: {
+          DEFAULT: 'hsl(var(--success))',
+          fg: 'hsl(var(--success-fg))'
+        },
+        info: {
+          DEFAULT: 'hsl(var(--info))',
+          fg: 'hsl(var(--info-fg))'
+        },
+        danger: {
+          DEFAULT: 'hsl(var(--danger))',
+          fg: 'hsl(var(--danger-fg))'
+        },
+        warning: {
+          DEFAULT: 'hsl(var(--warning))',
+          fg: 'hsl(var(--warning-fg))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          fg: 'hsl(var(--muted-fg))'
+        },
+        overlay: {
+          DEFAULT: 'hsl(var(--overlay))',
+          fg: 'hsl(var(--overlay-fg))'
+        }
       },
-    },
+      borderRadius: {
+        '3xl': 'calc(var(--radius) + 7.5px)',
+        '2xl': 'calc(var(--radius) + 5px)',
+        xl: 'calc(var(--radius) + 2.5px)',
+        lg: 'calc(var(--radius))',
+        md: 'calc(var(--radius) - 2.5px)',
+        sm: 'calc(var(--radius) - 5px)'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwindcss-react-aria-components')
+  ]
+}
+
+export default config
